16ccf15593b7ac077292be7e928e5ef116f5140c
Sascha Kiefer <perl@intertivityNOSP4M.com>
2006-07-23 23:36:56 +0000

    Import of ESSKAR/Win32-Crypt-0.00_004 from CPAN.
    gitpan-cpan-distribution: Win32-Crypt
    gitpan-cpan-version:      0.00_004
    gitpan-cpan-path:         ESSKAR/Win32-Crypt-0.00_004.tar.gz
    gitpan-cpan-author:       ESSKAR
    gitpan-cpan-maturity:     developer

--- lib/Win32/Crypt/API.pm
+++ lib/Win32/Crypt/API.pm
@@ -5,7 +5,7 @@ use warnings;
 use base qw/Exporter Win32::API::Interface/;
 
 use vars qw/$VERSION @EXPORT_OK %EXPORT_TAGS/;
-$VERSION = '0.00_003';
+$VERSION = '0.00_004';
 
 my %consts;
 
@@ -133,9 +133,115 @@ BEGIN {
         CRYPT_E_MISSING_PUBKEY_PARA        => 0x8009202C,
         CRYPT_E_OSS_ERROR                  => 0x80093000,
 
+        ERROR_NOT_ENOUGH_MEMORY => 8,
+        ERROR_INVALID_PARAMETER => 87,
+        ERROR_BUSY              => 107,
+        ERROR_MORE_DATA         => 234,
+        ERROR_NO_MORE_ITEMS     => 259,
+
+        NTE_BAD_SIGNATURE       => 0x80090006,
+        NTE_BAD_FLAGS           => 0x80090009,
+        NTE_NO_MEMORY           => 0x8009000E,
+        NTE_EXISTS              => 0x8009000F,
+        NTE_BAD_PROV_TYPE       => 0x80090014,
+        NTE_BAD_KEYSET          => 0x80090016,
+        NTE_PROV_TYPE_NOT_DEF   => 0x80090017,
+        NTE_PROV_TYPE_ENTRY_BAD => 0x80090018,
+        NTE_KEYSET_NOT_DEF      => 0x80090019,
+        NTE_KEYSET_ENTRY_BAD    => 0x8009001A,
+        NTE_PROV_TYPE_NO_MATCH  => 0x8009001B,
+        NTE_SIGNATURE_FILE_BAD  => 0x8009001C,
+        NTE_PROVIDER_DLL_FAIL   => 0x8009001D,
+        NTE_PROV_DLL_NOT_FOUND  => 0x8009001E,
+        NTE_BAD_KEYSET_PARAM    => 0x8009001F,
+        NTE_FAIL                => 0x80090020,
+
         CERT_STORE_CTRL_COMMIT_FORCE_FLAG             => 0x1,
         CERT_STORE_CTRL_COMMIT_CLEAR_FLAG             => 0x2,
         CERT_STORE_CTRL_INHIBIT_DUPLICATE_HANDLE_FLAG => 0x1,
+
+        PP_ENUMALGS            => 1,
+        PP_ENUMCONTAINERS      => 2,
+        PP_IMPTYPE             => 3,
+        PP_NAME                => 4,
+        PP_VERSION             => 5,
+        PP_CONTAINER           => 6,
+        PP_CHANGE_PASSWORD     => 7,
+        PP_KEYSET_SEC_DESCR    => 8,   # get/set security descriptor of keyset
+        PP_CERTCHAIN           => 9,   # for retrieving certificates from tokens
+        PP_KEY_TYPE_SUBTYPE    => 10,
+        PP_PROVTYPE            => 16,
+        PP_KEYSTORAGE          => 17,
+        PP_APPLI_CERT          => 18,
+        PP_SYM_KEYSIZE         => 19,
+        PP_SESSION_KEYSIZE     => 20,
+        PP_UI_PROMPT           => 21,
+        PP_ENUMALGS_EX         => 22,
+        PP_ENUMMANDROOTS       => 25,
+        PP_ENUMELECTROOTS      => 26,
+        PP_KEYSET_TYPE         => 27,
+        PP_ADMIN_PIN           => 31,
+        PP_KEYEXCHANGE_PIN     => 32,
+        PP_SIGNATURE_PIN       => 33,
+        PP_SIG_KEYSIZE_INC     => 34,
+        PP_KEYX_KEYSIZE_INC    => 35,
+        PP_UNIQUE_CONTAINER    => 36,
+        PP_SGC_INFO            => 37,
+        PP_USE_HARDWARE_RNG    => 38,
+        PP_KEYSPEC             => 39,
+        PP_ENUMEX_SIGNING_PROT => 40,
+        PP_CRYPT_COUNT_KEY_USE => 41,
+
+        CRYPT_FIRST    => 1,
+        CRYPT_NEXT     => 2,
+        CRYPT_SGC_ENUM => 4,
+
+        CRYPT_IMPL_HARDWARE  => 1,
+        CRYPT_IMPL_SOFTWARE  => 2,
+        CRYPT_IMPL_MIXED     => 3,
+        CRYPT_IMPL_UNKNOWN   => 4,
+        CRYPT_IMPL_REMOVABLE => 8,
+
+        CRYPT_SEC_DESCR => 0x00000001,
+        CRYPT_PSTORE    => 0x00000002,
+        CRYPT_UI_PROMPT => 0x00000004,
+
+        CRYPT_FLAG_PCT1    => 0x0001,
+        CRYPT_FLAG_SSL2    => 0x0002,
+        CRYPT_FLAG_SSL3    => 0x0004,
+        CRYPT_FLAG_TLS1    => 0x0008,
+        CRYPT_FLAG_IPSEC   => 0x0010,
+        CRYPT_FLAG_SIGNING => 0x0020,
+
+        CRYPT_SGC     => 0x0001,
+        CRYPT_FASTSGC => 0x0002,
+
+        PP_CLIENT_HWND         => 1,
+        PP_CONTEXT_INFO        => 11,
+        PP_KEYEXCHANGE_KEYSIZE => 12,
+        PP_SIGNATURE_KEYSIZE   => 13,
+        PP_KEYEXCHANGE_ALG     => 14,
+        PP_SIGNATURE_ALG       => 15,
+        PP_DELETEKEY           => 24,
+
+        PROV_RSA_FULL      => 1,
+        PROV_RSA_SIG       => 2,
+        PROV_DSS           => 3,
+        PROV_FORTEZZA      => 4,
+        PROV_MS_EXCHANGE   => 5,
+        PROV_SSL           => 6,
+        PROV_RSA_SCHANNEL  => 12,
+        PROV_DSS_DH        => 13,
+        PROV_EC_ECDSA_SIG  => 14,
+        PROV_EC_ECNRA_SIG  => 15,
+        PROV_EC_ECDSA_FULL => 16,
+        PROV_EC_ECNRA_FULL => 17,
+        PROV_DH_SCHANNEL   => 18,
+        PROV_SPYRUS_LYNKS  => 20,
+        PROV_RNG           => 21,
+        PROV_INTEL_SEC     => 22,
+        PROV_REPLACE_OWF   => 23,
+        PROV_RSA_AES       => 24,
 
     );
 
@@ -170,7 +276,22 @@ use constant \%consts;
 
 __PACKAGE__->generate(
     {
+        'advapi32' => [
+            [ 'CryptAcquireContext',          'PPPNN',  'I' ],
+            [ 'CryptContextAddRef',           'NPN',    'I' ],
+            [ 'CryptEnumProviders',           'NPNPPP', 'I' ],
+            [ 'CryptEnumProviderTypes',       'NPNPPP', 'I' ],
+            [ 'CryptGetDefaultProvider',      'NPNPP',  'I' ],
+            [ 'CryptGetProvParam',            'NNPPN',  'I' ],
+            [ 'CryptInstallDefaultContext',   'NNPNPP', 'I' ],
+            [ 'CryptReleaseContext',          'NN',     'I' ],
+            [ 'CryptSetProvider',             'PN',     'I' ],
+            [ 'CryptSetProviderEx',           'PNPN',   'I' ],
+            [ 'CryptSetProvParam',            'NNPN',   'I' ],
+            [ 'CryptUninstallDefaultContext', 'NNP',    'I' ],
+        ],
         'crypt32' => [
+
             [ 'CertAddStoreToCollection',      'NNNN',   'I' ],
             [ 'CertAddCertificateLinkToStore', 'NNNN',   'I' ],
             [ 'CertCloseStore',                'NN',     'I' ],
@@ -189,7 +310,7 @@ __PACKAGE__->generate(
             [ 'CertSetStoreProperty',          'NNNP',   'I' ],
             [ 'CertUnregisterPhysicalStore',   'PNP',    'I' ],
             [ 'CertUnregisterSystemStore',     'PN',     'I' ],
-        ]
+        ],
     }
 );
 
--- t/04certstore.t
+++ t/04certstore.t
@@ -11,3 +11,5 @@ ok($api = Win32::Crypt::API->new, 'new');
 my $certstore;
 ok($certstore = $api->CertOpenStore("System", 0, 0, 1 << 16, "MY"), 'CertOpenStore');
 ok($api->CertCloseStore($certstore, 0), 'CertCloseStore');
+
+# print STDERR join("\n", "\n", sort $api->constant_names );
